92fd667e54c2d2fff84ab0cfd05336cdb0e50b82
Nicholas Liu <liun0@sas.upenn.edu>
2022-07-11 15:42:43 -0400

    Splashback mass update (for Julia)

--- splashback_tools/profiles/mc_chain.py
+++ splashback_tools/profiles/mc_chain.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import Axes
+import warnings
 
 class McChain:
     def __init__(self, weights, loglikes, **samples):
@@ -31,11 +32,19 @@ class McChain:
         if not paramname in self.samples.keys():
             raise Exception(f"No parameter with name {paramname}")
         values = self.samples[paramname]
+        weights = self.weights
+
+        if np.any(np.isnan(values)):
+            warnings.warn(f"Samples of {paramname} contains NaN values")
+            if paramname not in self.sample_stats:
+                filt = ~np.isnan(values)
+                values = values[filt]
+                weights = weights[filt]
 
         #Lazy evaluate
         if paramname not in self.sample_stats:
-            mean = np.average(values, weights=self.weights, axis = 0)
-            var = np.average((values-mean)**2, weights=self.weights, axis = 0)
+            mean = np.average(values, weights=weights, axis = 0)
+            var = np.average((values-mean)**2, weights=weights, axis = 0)
             std = np.sqrt(var)
             self.sample_stats[paramname] = (mean, std)
         
@@ -47,20 +56,36 @@ class McChain:
                 raise Exception("Sample wrong length")
             self.samples[k] = v
 
-    def plot(self, paramname, label = None, bounds = None, ax = plt.gca()):
+    def plot_1d_posterior(self, paramname, ax : Axes, bounds = None, **kwargs):
         mean, std = self.get_samp_stats(paramname)
 
-        #Plot 3 sigma if no  bounds specified
+        #Plot 3 sigma if no bounds specified
         if bounds is None:
             bounds = (mean - 3 * std, mean + 3 * std)
         
         minx, maxx = bounds
         x = np.linspace(minx, maxx, 500)
-        if label is None:
-            ax.plot(x, sp.stats.norm.pdf(x, mean, std) * std)
-        else:
-            ax.plot(x, sp.stats.norm.pdf(x, mean, std) * std, label = label)
+        curve = ax.plot(x, sp.stats.norm.pdf(x, mean, std), **kwargs)[0]
         ax.yaxis.set_visible(False)
+        ax.legend()
+
+        #Plot histogram
+        values = self.samples[paramname]
+        filt = ~np.isnan(values)
+        values = values[filt]
+        weights = self.weights[filt]
+        counts, bins = np.histogram(values, bins = 30, weights = weights, range = (minx - 2 * std, maxx + 2 * std))
+        weights = counts/sum(counts)/(bins[1] - bins[0])
+        ax.hist(bins[:-1], bins, weights=weights, color = curve.get_color(), alpha = 0.5)
+
+    def plot_2d_scatter(self, p1, p2, ax : Axes, **kwargs):
+        v1 = self.samples[p1]
+        v2 = self.samples[p2]
+        filt = ~(np.isnan(v1) | np.isnan(v2))
+        v1 = v1[filt]
+        v2 = v2[filt]
+        weights = self.weights[filt]
+        ax.scatter(v1, v2, alpha=weights, **kwargs)
         ax.legend()
 
     def confidence_interval(self, paramname, stds = 1):
